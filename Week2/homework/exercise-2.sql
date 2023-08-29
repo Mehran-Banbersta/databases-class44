ea85ad01061c0e66c7a45b51ab72229928a07d45
mehran banbersta <mehranbanbersta@mehrans-mbp.kpn>
2023-08-29 22:00:27 +0200

    exercice-2 finished

--- Week2/homework/exercise-2.sql
+++ Week2/homework/exercise-2.sql
@@ -1,6 +1,14 @@
-CREATE TABLE IF NOT EXISTS research_Papers(
-  paper_id INTEGER PRIMARY KEY AUTO_INCREMENT,
-  paper_title VARCHAR(255) NOT NULL,
-  conference VARCHAR(255) NOT NULL,
-  publish_date DATE NOT NULL,
+-- 1. Create another table, called `research_Papers` with the following fields: `(paper_id, paper_title, conference, publish_date, ...)`
+CREATE TABLE research_papers (
+  paper_id int(11) NOT NULL AUTO_INCREMENT,
+  paper_title varchar(255) NOT NULL,
+  conference varchar(255) NOT NULL,
+  publish_date date NOT NULL
+);
+-- 2. What is the relationship between Authors and Research papers ? Make necessary changes to `authors` and `research_Papers` tables and add more tables if necessary.
+CREATE TABLE authors_research_Papers (
+  author_id INT,
+  paper_id INT,
+  FOREIGN KEY (author_id) REFERENCES authors(author_id),
+  FOREIGN KEY (paper_id) REFERENCES research_papers(paper_id)
 );
